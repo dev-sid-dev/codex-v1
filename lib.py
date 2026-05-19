3cb3be78ba2a4a9a620d22708da7b3f5826cf6bd
dsouzaim <dsouza@imdigital.com>
2026-05-19 18:17:10 -0300

    AGENTS.MD added

--- lib.py
+++ lib.py
@@ -4,3 +4,7 @@ def dummy_function():
 
 def another_dummy_function():
     print("This is another dummy message from lib.py")
+
+
+def third_dummy_function():
+    print("This is the third dummy message from lib.py")
